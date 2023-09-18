63486e67a0e4e75316e301acf1f4911479456a9a
kevin <Kevin_bazin@outlook.fr>
2023-09-18 16:04:17 +0200

    modif footer

--- public/wp-content/themes/blankslate-child/footer.php
+++ public/wp-content/themes/blankslate-child/footer.php
@@ -1,9 +1,8 @@
 </main>
-<?php get_sidebar(); ?>
 </div>
-<footer id="footer" role="contentinfo">
-<div id="copyright">
-&copy; <?php echo esc_html( date_i18n( __( 'Y', 'blankslate' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
+<footer>
+<div>
+<p>Mentions légales</p>
 </div>
 </footer>
 </div>
