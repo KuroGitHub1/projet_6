400064f9cd8a6188582ae445c6569d30200272e3
kevin <Kevin_bazin@outlook.fr>
2023-10-12 13:23:18 +0200

    mofif ajout admin

--- public/wp-content/themes/blankslate-child/functions.php
+++ public/wp-content/themes/blankslate-child/functions.php
@@ -28,7 +28,21 @@ if ( !function_exists('logo_support')){
     function logo_support(){
         add_theme_support('custom-logo', array(
             
-            'width' => 'thumbnail',
         ));
     }
+}
+
+// lien admin 
+add_filter( 'wp_nav_menu_items','add_admin_link', 10, 2 );
+
+function add_admin_link( $items, $args ) {
+
+    if (is_user_logged_in() && $args->theme_location == 'main-menu') {
+
+        $items = '<li><a href="'. get_admin_url() .'">Admin</a></li>' .$items;
+
+    }
+    
+    return $items;
+
 }
