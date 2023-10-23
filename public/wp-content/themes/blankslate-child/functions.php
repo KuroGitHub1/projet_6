4c5f89258e6f2b280be5814178a355c3f264dc7c
kevin <Kevin_bazin@outlook.fr>
2023-10-23 19:07:49 +0200

    modif pour formulaire 1

--- public/wp-content/themes/blankslate-child/functions.php
+++ public/wp-content/themes/blankslate-child/functions.php
@@ -16,10 +16,11 @@ add_filter( 'locale_stylesheet_uri', 'chld_thm_cfg_locale_css' );
 
 if ( !function_exists( 'chld_thm_cfg_parent_css' ) ):
     function chld_thm_cfg_parent_css() {
-        wp_enqueue_style( 'chld_thm_cfg_parent', trailingslashit( get_template_directory_uri() ) . 'style.css', array(  ),'1.0' );
+        wp_enqueue_style( 'chld_thm_cfg_parent', trailingslashit( get_stylesheet_directory_uri() ) . 'style.css', array(  ),'1.0' );
     }
 endif;
 add_action( 'wp_enqueue_scripts', 'chld_thm_cfg_parent_css', 10 );
+add_action( 'admin_enqueue_scripts', 'chld_thm_cfg_parent_css', 10 );
 
 // END ENQUEUE PARENT ACTION
 // ajout support du logo
@@ -46,3 +47,36 @@ function add_admin_link( $items, $args ) {
     return $items;
 
 }
+
+add_action("add_meta_boxes","image_formulaire",10,2);
+
+    function image_formulaire($post_type, $post){
+        $titre = $post->post_title;
+        if ($titre =="Commander"){
+            add_meta_box(
+                "champ_image",
+                "champ image",
+                "afficher_meta_image",
+            );
+        }
+    }
+
+    function afficher_meta_image($post){
+       echo('<div class="bloc_images">');
+            for($i=1; $i<=4; $i++){
+                $bloc_image=<<<EOD
+                <div class="bloc_image">
+                    <div class="nom_img">
+                        <input type="text" placeholder="titre de l'image $i" name="form_image_title_$i"/>
+                    </div>
+                    <div class="zone_img">
+                        <input type="hidden" name="form_image_$i"/>
+                        <input class="img_upload" type="button" value="ajouter une image"/>
+                        <img src="">  
+                    </div>
+                </div>
+                EOD;
+                echo($bloc_image);
+            }
+       echo('</div>');
+    }
